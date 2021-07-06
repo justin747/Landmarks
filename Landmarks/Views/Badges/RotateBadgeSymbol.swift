6986b31e840d1797619829ffd85da66f5255f490
Justin747 <justin747@gmail.com>
2021-07-05 19:57:22 -0700

    Started Section 2
    Went back to section 2
    
    * Added custom hexagon badge

---
+++ Landmarks/Views/Badges/RotateBadgeSymbol.swift
@@ -0,0 +1,25 @@
+//
+//  RotateBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Justin747 on 7/5/21.
+//
+
+import SwiftUI
+
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
