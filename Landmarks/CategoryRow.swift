13c3035d2a08e944e67f0f20d12615039f785f02
Justin747 <justin747@gmail.com>
2021-07-06 20:45:07 -0700

    Added Categories
    Completed CategoriesList from section 2

---
+++ Landmarks/CategoryRow.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryRow.swift
+//  Landmarks
+//
+//  Created by Justin747 on 7/6/21.
+//
+
+import SwiftUI
+
+struct CategoryRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryRow_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryRow()
+    }
+}
