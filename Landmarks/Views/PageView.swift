67a2dd86359a341004be5fd830805533f98db4ce
Justin747 <justin747@gmail.com>
2021-07-09 16:25:31 -0700

    Framework Integration - Section 1 complete
    Added PageViewController via UIKit

---
+++ Landmarks/Views/PageView.swift
@@ -0,0 +1,24 @@
+//
+//  PageView.swift
+//  Landmarks
+//
+//  Created by Justin747 on 7/9/21.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
---
+++ Landmarks/Views/PageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Justin747 on 7/9/21.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll, navigationOrientation: .horizontal)
+        
+        return pageViewController
+    }
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
