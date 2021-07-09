e1383bd26661733d03015b2c122e975ae18962a3
Justin747 <justin747@gmail.com>
2021-07-08 21:29:33 -0700

    App Design and Layout - Section 1 complete
    Added lists to main page and profile modal sheet

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,26 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Justin747 on 7/8/21.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20){
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
+
