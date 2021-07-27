83d084be7027d0204e38594ba01e66fc03d1de9a
Justin747 <justin747@gmail.com>
2021-07-26 22:47:21 -0700

    MacOS component added

---
+++ MacOSLandmarks/MacOSLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacOSLandmarksApp.swift
+//  MacOSLandmarks
+//
+//  Created by Justin747 on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct MacOSLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
